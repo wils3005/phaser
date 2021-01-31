2bdb3ef9b416d5e7bde00301b341196d7b74a9d6
Jack Wilson <wils3005@gmail.com>
2021-01-30 23:20:37 -0500

    Sun, 31 Jan 2021 04:20:37 GMT
     21 files changed, 2164 insertions(+), 266 deletions(-)

---
+++ public/main.d.ts
@@ -0,0 +1,5 @@
+import Peer from "peerjs";
+import Phaser from "phaser";
+declare const peer: Peer;
+declare const game: Phaser.Game;
+export { game, peer };
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+// import Peer from "peerjs";
+// import Phaser from "phaser";
+
+const {
+  constructor: { name },
+  location: { pathname },
+} = globalThis;
+
+const peer = new Peer();
+
+class Scene extends Phaser.Scene {
+  constructor() {
+    super("@wilsjs/phaser");
+  }
+
+  preload(): void {
+    // TODO
+  }
+
+  create(): void {
+    // TODO
+  }
+}
+
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  backgroundColor: "#125555",
+  width: 800,
+  height: 600,
+  scene: Scene,
+});
+
+function log(error?: Error): void {
+  const { stack } = new Error();
+  const a = stack?.split("\n") || [];
+  const s = a[1];
+
+  const message = {
+    globalThis: name,
+    location: pathname,
+    function: String(/at (.+) /.exec(s)?.pop()),
+  };
+
+  error ? console.error({ ...message, error }) : console.info(message);
+}
+
+function onWindowLoad(): void {
+  log();
+}
+
+globalThis.addEventListener("load", onWindowLoad);
+
+export { game, peer };
